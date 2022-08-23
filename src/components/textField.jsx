006b1ccf9035a46773014f1c391fbd7eaac5f8c3
realing29 <realing29@gmail.com>
2022-08-23 20:41:36 +0500

    complete lesson 7 forms

---
+++ src/components/textField.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+const TextField = ({ label, type, name, value, onChange, error }) => {
+  console.log(error);
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input type={type} id={name} name={name} value={value} onChange={onChange} />
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+TextField.defaultProps = {
+  type: "text",
+};
+
+TextField.propTypes = {
+  label: PropTypes.string,
+  type: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  error: PropTypes.string,
+};
+
+export default TextField;
--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,5 +1,56 @@
+import { useEffect, useState } from "react";
+import TextField from "../components/textField";
+
 const Login = () => {
-  return <h1>Login</h1>;
+  const [data, setData] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+  };
+
+  useEffect(() => {
+    validate();
+  }, [data]);
+
+  const validate = () => {
+    const errors = {};
+    for (const fieldName in data) {
+      if (data[fieldName].trim() === "") {
+        errors[fieldName] = `${fieldName} обязательно для заполнения`;
+      }
+    }
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validate();
+    if (!isValid) return;
+    console.log("🚀 ~ file: login.jsx ~ line 14 ~ handleSubmit ~ data", data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Электронная почта"
+        name="email"
+        value={data.email}
+        onChange={handleChange}
+        error={errors.email}
+      />
+      <TextField
+        label="Пароль"
+        type="password"
+        name="password"
+        value={data.password}
+        onChange={handleChange}
+        error={errors.password}
+      />
+      <button>Submit</button>
+    </form>
+  );
 };
 
 export default Login;
