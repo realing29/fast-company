2624466a5b4e5902a5b74b1a8655069bfbced446
realing29 <realing29@gmail.com>
2022-08-25 01:02:39 +0500

    completed last lesson topic forms part 1

--- src/components/textField.jsx
+++ src/components/textField.jsx
@@ -1,11 +1,34 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 const TextField = ({ label, type, name, value, onChange, error }) => {
-  console.log(error);
+  const [showPassword, setShowPassword] = useState(false);
+  const getInputClasses = () => {
+    return "form-control" + (error ? " is-invalid" : "");
+  };
+
+  const toggleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
   return (
-    <div>
+    <div className="mb-4">
       <label htmlFor={name}>{label}</label>
-      <input type={type} id={name} name={name} value={value} onChange={onChange} />
-      {error && <p>{error}</p>}
+      <div className="input-group has-validation">
+        <input
+          type={showPassword ? "text" : type}
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+          className={getInputClasses()}
+        />
+        {type === "password" && (
+          <button className="btn btn-outline-secondary" type="button" onClick={toggleShowPassword}>
+            <i className={"bi bi-eye" + (showPassword ? "-slash" : "")}></i>
+          </button>
+        )}
+        {error && <div className="invalid-feedback">{error}</div>}
+      </div>
     </div>
   );
 };
--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import TextField from "../components/textField";
+import { validator } from "../utils/validator";
 
 const Login = () => {
   const [data, setData] = useState({ email: "", password: "" });
@@ -9,20 +10,43 @@ const Login = () => {
     setData((prevState) => ({ ...prevState, [target.name]: target.value }));
   };
 
+  const validatorConfig = {
+    email: {
+      isRequired: {
+        message: "Электронная почта обязательна для заполнения",
+      },
+      isEmail: {
+        message: "Email введен не корректно",
+      },
+    },
+    password: {
+      isRequired: {
+        message: "Пароль обязателен для заполнения",
+      },
+      isCapitalSymbol: {
+        message: "Пароль должен содержать хотя бы одну заглавную букву",
+      },
+      isContainDigit: {
+        message: "Пароль должен содержать хотя бы цифру",
+      },
+      min: {
+        message: "Пароль должен состоять минимум из 8 символов",
+        value: 8,
+      },
+    },
+  };
+
   useEffect(() => {
     validate();
   }, [data]);
 
   const validate = () => {
-    const errors = {};
-    for (const fieldName in data) {
-      if (data[fieldName].trim() === "") {
-        errors[fieldName] = `${fieldName} обязательно для заполнения`;
-      }
-    }
+    const errors = validator(data, validatorConfig);
     setErrors(errors);
     return Object.keys(errors).length === 0;
   };
+
+  const isValid = Object.keys(errors).length === 0;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -32,24 +56,33 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        label="Электронная почта"
-        name="email"
-        value={data.email}
-        onChange={handleChange}
-        error={errors.email}
-      />
-      <TextField
-        label="Пароль"
-        type="password"
-        name="password"
-        value={data.password}
-        onChange={handleChange}
-        error={errors.password}
-      />
-      <button>Submit</button>
-    </form>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 shadow p-4">
+          <h3 className="mb-4">Login</h3>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              label="Электронная почта"
+              name="email"
+              value={data.email}
+              onChange={handleChange}
+              error={errors.email}
+            />
+            <TextField
+              label="Пароль"
+              type="password"
+              name="password"
+              value={data.password}
+              onChange={handleChange}
+              error={errors.password}
+            />
+            <button type="submit" disabled={!isValid} className="btn btn-primary w-100 mx-auto">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
