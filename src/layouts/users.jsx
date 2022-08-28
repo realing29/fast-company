3950107b9e912a5aa34ec8abb1f0c1b03e9e9c5e
realing29 <realing29@gmail.com>
2022-08-29 00:41:36 +0500

    completed homework topic forms part 2 lite fix

--- src/layouts/users.jsx
+++ src/layouts/users.jsx
@@ -6,9 +6,10 @@ import UsersListPage from "../components/page/usersListPage";
 const Users = () => {
   const params = useParams();
   const { userId } = params;
+
   return (
     <Switch>
-      <Route path="/users/:userId?/edit" component={UserEditPage} />
+      <Route path="/users/:userId/edit" component={UserEditPage} />
       <Route
         path="/users/:userId?"
         render={() => <>{userId ? <UserPage userId={userId} /> : <UsersListPage />}</>}
